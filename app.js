ec6fcd517789d419ec32f2ecee256700ae361fb9
Larry Lade <lplade@users.noreply.github.com>
2016-04-05 22:39:32 -0500

    Random chance generator working

--- app.js
+++ app.js
@@ -29,8 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 //app.use('/users', users);
 
-
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	var err = new Error('Not Found');
@@ -64,12 +62,14 @@ app.use(function (err, req, res, next) {
 
 module.exports = app;
 
-// END WEB SERVER ROUTING
+// *** END WEB SERVER ROUTING ***
 
 // Main twitterbot logic -- split into own file?
 
 //TODO store/retrieve data in MongoDB
 
+// const for non-event "error"
+var SILENCE = "SILENCE";
 
 //Pull in array of monsters
 var monsterObj = require('./monsters.json');
@@ -77,12 +77,11 @@ var monsterObj = require('./monsters.json');
 //Prototype for monster object
 //This helps the code inspector understand object properties
 //http://stackoverflow.com/questions/5873624/parse-json-string-into-a-particular-object-prototype-in-javascript
-function Mobj(obj)
-{
+function Mobj(obj) {
 	this.quantity = 1;
 	this.monster = "normal man";
 	for (var prop in obj) {
-		this[prop] = obj[prop];
+		this[prop] = obj[prop]; //hasOwnProperty needed?
 	}
 }
 
@@ -96,11 +95,11 @@ var timer = later.setInterval(run, textSched);
 // Instantiate Twitter keys
 // https://github.com/ttezel/twit
 var t = new Twit({
-	consumer_key:         process.env.CONSUMER_KEY,
-	consumer_secret:      process.env.CONSUMER_SECRET,
-	access_token:         process.env.ACCESS_TOKEN,
-	access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
-	timeout_ms:           60*1000
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+	timeout_ms: 60 * 1000
 });
 
 // this was pretty useful
@@ -110,14 +109,19 @@ var t = new Twit({
 // https://github.com/caolan/async#waterfall
 function run() {
 	async.waterfall([
+			encounterCheck,
 			getMonster,
 			formatTweet
 			//postTweet
 		],
 		function (err, result) {
 			if (err) {
-				console.log("There was an error posting to Twitter: ", err);
-			} else {
+				if (err == SILENCE){
+					console.info(".");
+				} else {
+					console.warn("There was an error posting to Twitter: ", err);
+				}
+			} else if (result) {
 				console.log("Tweet successful!");
 				console.log("Tweet: ", result);
 			}
@@ -125,12 +129,22 @@ function run() {
 	);
 }
 
+function encounterCheck(callback) {
+	randRoll = Math.floor(Math.random() * 10); //random integer 0-9
+	if (randRoll == 0) {
+		console.log("Go!");
+		callback(null); // proceed with Tweet generation
+	} else {
+		callback(SILENCE);
+	}
+}
+
 function getMonster(callback) {
 	//TODO randomly pull an entry from monsters.json
 	//http://stackoverflow.com/questions/10011011/using-node-js-how-do-i-read-a-json-object-into-server-memory
 	console.log("Get monster function");
 	//http://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
-	var randMonster = function(){ //closure so it doesn't re-use "random" number
+	var randMonster = function () { //closure so it doesn't re-use "random" number
 		return monsterObj[Math.floor(Math.random() * monsterObj.length)];
 	}();
 	var monster = new Mobj(randMonster);
@@ -148,25 +162,25 @@ function formatTweet(monst, callback) {
 	//TODO check if .quanity and .monster are available?
 	//Part 2: Group name, quantity, or article
 	if (monst.quantity == 1) {
-		firstLetter = monst.monster.substring(0,1).toUpperCase();
+		firstLetter = monst.monster.substring(0, 1).toUpperCase();
 		//http://stackoverflow.com/questions/26926994/javascript-check-if-character-is-a-vowel
 		isVowel = firstLetter == "A" ||
-				firstLetter == "E" ||
-				firstLetter == "I" ||
-				firstLetter == "O" ||
-				firstLetter == "U";
+			firstLetter == "E" ||
+			firstLetter == "I" ||
+			firstLetter == "O" ||
+			firstLetter == "U";
 		article = isVowel ? "an " : "a ";
 		message += article;
-	} else if ( Number.isInteger(monst.quantity)){
+	} else if (Number.isInteger(monst.quantity)) {
 		//TODO convert low-value integers to number words
 		numString = " " + monst.quantity + " ";
 		message += numString;
-	} else if (monst.quantity.substring(0,1) == '%') {
+	} else if (monst.quantity.substring(0, 1) == '%') {
 		//TODO use % to signify a random range expression i.e. %2d4
 		throw "Not ready yet";
 	} else {
 		// Just use whatever content is, plus appropriate article
-		firstLetter = monst.monster.substring(0,1).toUpperCase();
+		firstLetter = monst.monster.substring(0, 1).toUpperCase();
 		//http://stackoverflow.com/questions/26926994/javascript-check-if-character-is-a-vowel
 		isVowel = firstLetter == "A" ||
 			firstLetter == "E" ||
@@ -180,12 +194,12 @@ function formatTweet(monst, callback) {
 	}
 	//Part 3: the actual monster name
 	message += monst.monster;
-	console.log("'"+message+"'");
+	console.log("'" + message + "'");
 	callback(null, message);
 }
 
 function postTweet(tweetMessage, callback) {
-	t.post('statuses/update', { status: tweetMessage }, function(err, data, response) {
+	t.post('statuses/update', {status: tweetMessage}, function (err, data, response) {
 		callback(null, result);
 	});
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'You encounter...' });
+router.get('/', function (req, res, next) {
+	res.render('index', {title: 'You encounter...'});
 });
+
+//TODO authenticate user
+//TODO allow add/remove/edit database entries
 
 module.exports = router;
