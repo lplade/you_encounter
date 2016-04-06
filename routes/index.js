351d3292501adbc46a711831349c88a558b39c43
Larry Lade <lplade@users.noreply.github.com>
2016-04-06 02:34:51 -0500

    Tweaking

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function (req, res, next) {
 	res.render('index', {title: 'You encounter...'});
 });
 
+//TODO show some behind the scenes check info
 //TODO authenticate user
 //TODO allow add/remove/edit database entries
 
